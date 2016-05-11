85ed525ef55f9b0c72d32cba4e9366a80d513094
ferMartz <fernandomvc@gmail.com>
2016-05-11 13:55:37 -0500

    fixed error message email

--- app_server/controllers/company.js
+++ app_server/controllers/company.js
@@ -8,6 +8,7 @@ module.exports.contact = function(req, res){
 res.render('contact', { title: 'Contact' });
 };
 
+
 /* Get terms */
 module.exports.terms = function(req, res){
 res.render('terms', { title: 'Terms & Conditions' });
